c2b0dd7cc375146246add4aa0a3ab8c66c8aaf97
LRPG2410 <lrporras24.10@gmail.com>
2026-04-18 21:34:58 -0600

    Primer commit: Estructura base de la interfaz de login

---
+++ src/main/java/com/mycompany/laboratorio2/LABORATORIO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2;
+
+/**
+ *
+ * @author lrpor
+ */
+public class LABORATORIO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
